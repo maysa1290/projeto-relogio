0669ae0542ab55e80cf5a933d8475d96ad88b938
jaciane1990 <jaciane.izidro@gmail.com>
2025-07-25 10:36:30 -0300

    atualizado

--- src/utils/tempo.ts
+++ src/utils/tempo.ts
@@ -1,4 +1,14 @@
 export function obterHorarioAtual(): string {
   const agora = new Date();
-  return agora.toLocaleTimeString('pt-BR', {hour12: false});
+  return agora.toLocaleTimeString('pt-BR', { hour12: false });
+}
+
+export function obterDataAtual(): string {
+  const agora = new Date();
+  return agora.toLocaleDateString('pt-BR', {
+    weekday: 'long',  // segunda-feira, terça-feira, etc.
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 }
