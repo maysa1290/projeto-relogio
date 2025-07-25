057e25bc45483b3337d25150d19f01157fcc1222
jaciane1990 <jaciane.izidro@gmail.com>
2025-07-25 10:56:04 -0300

    atual

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-// src/App.tsx
-
+import './App.css';
 import { useEffect, useState } from 'react';
 import { obterHorarioAtual } from './utils/tempo';
+import { obterDataAtual } from './utils/tempo';
 
 function App() {
+
   const [horario, setHorario] = useState(obterHorarioAtual());
+  const [data, setData] = useState(obterDataAtual());
 
   useEffect(() => {
     const intervalo = setInterval(() => {
       setHorario(obterHorarioAtual());
-    }, 1000); // atualiza a cada segundo
-
-    return () => clearInterval(intervalo); // limpa o intervalo ao desmontar
+      setData(obterDataAtual());
+    }, 1000);
+    return () => clearInterval(intervalo);
   }, []);
 
   return (
     <>
       <h1>Horário Atual</h1>
+      <h2>{data}</h2>
       <h1>{horario}</h1>
     </>
   );
