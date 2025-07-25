1afed80bb889670f88870d759a6314e7aea16f42
jaciane1990 <jaciane.izidro@gmail.com>
2025-07-25 10:23:09 -0300

    horarioatual

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import { obterHorarioAtual } from './utils/tempo'
+// src/App.tsx
+
+import { useEffect, useState } from 'react';
+import { obterHorarioAtual } from './utils/tempo';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [horario, setHorario] = useState(obterHorarioAtual());
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setHorario(obterHorarioAtual());
+    }, 1000); // atualiza a cada segundo
+
+    return () => clearInterval(intervalo); // limpa o intervalo ao desmontar
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Horário Atual</h1>
+      <h1>{horario}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
