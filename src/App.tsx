c17780d781c664b3ec9d0ed49cda8aefff0e3b76
maysa1290 <maysa.github@gmail.com>
2025-07-30 01:49:49 -0300

    Corrige App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { obterHorarioAtual } from './utils/tempo';
-import { obterDataAtual } from './utils/tempo';
+import { obterHorarioAtual, obterDataAtual } from './utils/tempo';
 
 function App() {
-
   const [horario, setHorario] = useState(obterHorarioAtual());
   const [data, setData] = useState(obterDataAtual());
 
@@ -20,7 +18,7 @@ function App() {
     <>
       <h1>Horário Atual</h1>
       <h2>{data}</h2>
-      <h1>{horario}</h1>
+      <h2>{horario}</h2>
     </>
   );
 }
